ce0cfb16408869047104245e48a2a840e16de97c
jangsh4752 <jangsh4752@naver.com>
2025-08-25 17:52:27 +0900

    0825 용어 사전 데이터 추가 및 카테고리화

--- jang/insurance_project/insurance_app/pinecone_client.py
+++ jang/insurance_project/insurance_app/pinecone_client.py
@@ -4,14 +4,14 @@ import os
 pc = Pinecone(api_key=os.environ["PINECONE_API_KEY"])
 
 # 환경변수로 인덱스 이름 관리
-INDEX_NAME = os.environ.get("PINECONE_INDEX_NAME", "insurance-clauses-new")
+INDEX_NAME = os.environ.get("PINECONE_INDEX_NAME", "insurance-clauses-second")
 
 def create_index():
     existing_indexes = [i["name"] for i in pc.list_indexes()]
     if INDEX_NAME not in existing_indexes:
         pc.create_index(
             name=INDEX_NAME,
-            dimension=768,  # 업로드 모델과 일치하도록 768로 변경
+            dimension=1024,  # 업로드 모델과 일치하도록 768로 변경
             metric="cosine",
             spec=ServerlessSpec(
                 cloud='aws',
